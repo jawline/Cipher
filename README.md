b3cd550bd977e07c03dab665e19d5e9000a0c256
Blake Loring <blake_l@parsed.uk>
2016-01-27 18:28:55 +0000

    Workign interface

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 #Cipher Playground
 
 Playground for implementing crypto stuff
+
+#Controls
+make_key will generate a key in base64
+encrypt KEY TEXT will generate ciphertext base64 encoded
+decrypt KEY CIPHERTEXT will output the original data
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate rand;
+extern crate rustc_serialize;
 
 mod stream;
 mod keys;
 
+use rustc_serialize::base64::{STANDARD, FromBase64, ToBase64};
 use std::env;
 use stream::*;
 
@@ -13,18 +15,20 @@ fn main() {
 
 	let command = &args[1];
 
-	let key = keys::create(16);
-
-	println!("{}", String::from_utf8(stream::decrypt(&stream::encrypt("hello".as_bytes(), &key), &key)).unwrap());
 
 	match command as &str {
 		"encrypt" => {
-			let key = &args[2];
+			let key_text = &args[2].from_base64().unwrap();
 			let text = &args[3];
+			println!("{}", &stream::encrypt("hello".as_bytes(), &key_text).to_base64(STANDARD));
 		},
 		"decrypt" => {
-			let key = &args[2];
-			let text = &args[3];
+			let key_text = &args[2].from_base64().unwrap();
+			let text = &args[3].from_base64().unwrap();
+			println!("{}", String::from_utf8(stream::decrypt(&text, &key_text)).unwrap());
+		},
+		"make_key" => {
+			println!("{}", &keys::create(16).to_base64(STANDARD))
 		},
 		_ => { println!("Unknown"); }
 	}
