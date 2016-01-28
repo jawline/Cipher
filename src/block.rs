03a912f3ace7d689eadfcf5fc48cddeaa3908685
Blake Loring <blake_l@parsed.uk>
2016-01-28 10:46:24 +0000

    Update block

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,7 @@
 /**
  * Block will be able to store arbitrary blocks of bits
  */
+ use std::ops::BitXor;
 
 struct Block {
 	bytes: Vec<u8>,
@@ -9,18 +10,32 @@ struct Block {
 
 impl Block {
 	pub fn new(size: usize) -> Block {
-		Block{
-			Vec::new(),
-			size
+		Block {
+			bytes: Vec::new(),
+			size: size
 		}
 	}
 	
 	fn set(&mut self, bytes: &[u8]) {
+		self.bytes = bytes.iter().map(|&x| x).collect();
 	}
 }
 
 impl BitXor for Block {
+	type Output = Block;
+
 	fn bitxor(self, rhs: Block) -> Block {
-		self.bytes
+		if rhs.size != self.size {
+			self
+		} else {
+			let mut newBytes = Vec::new();
+			for i in 0..self.bytes.len() {
+				newBytes.push(self.bytes[i] ^ rhs.bytes[i]);
+			}
+
+			let mut result = Block::new(self.size);
+			result.set(&newBytes);
+			result
+		}
 	}
 }
