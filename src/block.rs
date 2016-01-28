5f5a2f760631ccabd0532c760e15f44febed5f64
Blake Loring <blake_l@parsed.uk>
2016-01-28 11:12:57 +0000

    Start on blocks_from_bytes

--- src/block.rs
+++ src/block.rs
@@ -3,13 +3,21 @@
  */
  use std::ops::BitXor;
 
-struct Block {
+pub struct Block {
 	bytes: Vec<u8>,
 	size: usize
 }
 
-impl Block {
+const B1: u8 = 1;
+const B2: u8 = 2;
+const B3: u8 = 4;
+const B4: u8 = 8;
+const B5: u8 = 16;
+const B6: u8 = 32;
+const B7: u8 = 64;
+const B8: u8 = 128;
 
+impl Block {
 	pub fn new(size: usize) -> Block {
 		Block {
 			bytes: Vec::new(),
@@ -35,4 +43,12 @@ impl BitXor for Block {
 			Block::from(self.size, &self.bytes.iter().zip(rhs.bytes.iter()).map(|(lhs, rhs)| lhs ^ rhs).collect::<Vec<u8>>())
 		}
 	}
+}
+
+pub fn blocks_from_bytes(bytes: &[u8], block_size: usize) -> Vec<Block> {
+	let mut current_byte = 0;
+	let mut bytes_from_last_block = 0;
+	let mut blocks = Vec::new();
+
+	blocks
 }
